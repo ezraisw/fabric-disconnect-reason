65e03904cea2e683628d6ec7b1ce83d057138584
Ezra Indrastata <ezra.inspw@gmail.com>
2021-07-20 02:36:09 +0700

    Initial implementation

---
+++ src/main/java/com/inspw/mcmod/disconnectreason/mixin/ServerPlayNetworkHandlerMixin.java
@@ -0,0 +1,29 @@
+package com.inspw.mcmod.disconnectreason.mixin;
+
+import net.minecraft.network.MessageType;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.UUID;
+
+@Mixin(ServerPlayNetworkHandler.class)
+public class ServerPlayNetworkHandlerMixin {
+    @Redirect(
+            method = "onDisconnected(Lnet/minecraft/text/Text;)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;broadcastChatMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/MessageType;Ljava/util/UUID;)V")
+    )
+    public void onDisconnectedBroadcastChatMessageProxy(PlayerManager playerManager, Text _message, MessageType type, UUID sender, Text reason) {
+        MutableText message = ((MutableText) _message)
+                .append(new LiteralText(" ("))
+                .append(reason)
+                .append(new LiteralText(")"));
+
+        playerManager.broadcastChatMessage(message, type, sender);
+    }
+}
