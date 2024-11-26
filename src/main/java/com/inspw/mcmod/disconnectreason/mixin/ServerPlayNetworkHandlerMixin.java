bc37a6b93e844c737817d1b1fddca2d6bbbd64a1
pwnedgod <ezra1551@gmail.com>
2024-11-27 04:29:12 +0900

    Update mixin to work on 1.20 signatures

--- src/main/java/com/inspw/mcmod/disconnectreason/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/com/inspw/mcmod/disconnectreason/mixin/ServerPlayNetworkHandlerMixin.java
@@ -1,29 +1,22 @@
 package com.inspw.mcmod.disconnectreason.mixin;
 
-import net.minecraft.network.MessageType;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
-import net.minecraft.text.LiteralText;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.UUID;
-
 @Mixin(ServerPlayNetworkHandler.class)
 public class ServerPlayNetworkHandlerMixin {
-    @Redirect(
-            method = "onDisconnected(Lnet/minecraft/text/Text;)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;broadcast(Lnet/minecraft/text/Text;Lnet/minecraft/network/MessageType;Ljava/util/UUID;)V")
-    )
-    public void onDisconnectedBroadcastProxy(PlayerManager playerManager, Text _message, MessageType type, UUID sender, Text reason) {
+    @Redirect(method = "onDisconnected(Lnet/minecraft/text/Text;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;broadcast(Lnet/minecraft/text/Text;Z)V"))
+    public void cleanUpBroadcastProxy(PlayerManager playerManager, Text _message, boolean overlay, Text reason) {
         MutableText message = ((MutableText) _message)
-                .append(new LiteralText(" ("))
+                .append(" (")
                 .append(reason)
-                .append(new LiteralText(")"));
+                .append(")");
 
-        playerManager.broadcast(message, type, sender);
+        playerManager.broadcast(message, overlay);
     }
 }
